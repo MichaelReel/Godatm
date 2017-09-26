152ce4cb03e83b5e3f4afbfa7de36b045707cf1b
Michael Reel <mykilr@gmail.com>
2017-09-26 21:31:58 +0100

    Added the perlin noise reference code. Extended the generated map area. Use perlin to select tiles.

--- WaterGrassTiles.gd
+++ WaterGrassTiles.gd
@@ -1,17 +1,16 @@
 extends TileMap
 
-# class member variables go here, for example:
-# var a = 2
-# var b = "textvar"
-
 export var gridSize = Vector2()
 
 func _ready():
+	
+	var perlin = load("res://PerlinRef.gd").new(64, 64, 10)
 	# Called every time the node is added to the scene.
 	# Initialization here
 	var tilemap = self
 	var tileset = tilemap.get_tileset()
 	var water = tileset.find_tile_by_name("WaterGrass_00")
+	var grass = tileset.find_tile_by_name("Grass_01")
 	
 	var grid_start_x = 0
 	var grid_start_y = 0
@@ -20,4 +19,7 @@ func _ready():
 	
 	for y in range(grid_start_y, grid_end_y):
 		for x in range(grid_start_x, grid_end_x):
-			var cell = tilemap.set_cell(x, y, water)
+			if perlin.getHash(x, y) >= 0.05:
+				tilemap.set_cell(x, y, water)
+			else:
+				tilemap.set_cell(x, y, grass)
