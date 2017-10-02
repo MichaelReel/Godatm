6ca9ba11a4e5de3c6707391b7373403935e008f9
Michael Reel <mykilr@gmail.com>
2017-10-02 22:48:28 +0100

    Included tileset scene file (for editing the tileset). Added tree tiles. Modified the map creation to take different layers. Changed the icon.

--- WaterGrassTiles.gd
+++ WaterGrassTiles.gd
@@ -1,11 +1,16 @@
-extends TileMap
+extends Node2D
 
 export var gridSize = Vector2()
+export (TileSet) var tileset
 
-var grid_start_x;
-var grid_start_y;
-var grid_end_x;
-var grid_end_y;
+const BASE_TILE_MAP = 0
+const SOLID_TILE_MAP = 1
+
+var grid_start_x
+var grid_start_y
+var grid_end_x
+var grid_end_y
+var tilemaps
 
 func _ready():
 	
@@ -14,35 +19,52 @@ func _ready():
 	grid_end_x = int(gridSize.x)
 	grid_end_y = int(gridSize.y)
 	
-	var tilemap = self
-	var tileset = tilemap.get_tileset()
+	tilemaps = self.get_children()
+	
+	var basemap = tilemaps[BASE_TILE_MAP]
+	var solidmap = tilemaps[SOLID_TILE_MAP]
+	
 	var water = tileset.find_tile_by_name("WaterGrass_00")
 	var grass = tileset.find_tile_by_name("Grass_01")
+	var tree = tileset.find_tile_by_name("Tree_00")
 	
-	basic_perlin_fill(tilemap, grass, water)
+	basic_perlin_fill(tilemaps, grass, water, tree)
+	
+	# Index tileset groups
 	
 	var waterGrass = []
 	for i in range(16):
 		waterGrass.append(tileset.find_tile_by_name("WaterGrass_%02d" % i))
-	
-	simple_marching_squares(tilemap, grass, waterGrass)
+
+	var tree = []
+	for i in range(16):
+		waterGrass.append(tileset.find_tile_by_name("Tree_%02d" % i))
 	
 	var grasses = []
 	for i in range(1, 9):
 		grasses.append(tileset.find_tile_by_name("Grass_%02d" % i))
 	
-	randomise_grass(tilemap, grass, grasses)
-
-func basic_perlin_fill(tilemap, grass, water):
+	simple_marching_squares(basemap, grass, waterGrass)
 	
-	var perlin = load("res://PerlinRef.gd").new(64, 64, 10)
+	randomise_grass(basemap, grass, grasses)
+
+func basic_perlin_fill(tilemaps, grass, water, tree):
+	
+	var basemap = tilemaps[BASE_TILE_MAP]
+	var solidmap = tilemaps[SOLID_TILE_MAP]
+	
+	var perlinRef = load("res://PerlinRef.gd")
+	var base = perlinRef.new(64, 64, 10)
+	var solid = perlinRef.new(64, 64, 5)
 	
 	for y in range(grid_start_y, grid_end_y):
 		for x in range(grid_start_x, grid_end_x):
-			if perlin.getHash(x, y) >= 0.05:
-				tilemap.set_cell(x, y, water)
+			if base.getHash(x, y) >= 0.05:
+				basemap.set_cell(x, y, water)
 			else:
-				tilemap.set_cell(x, y, grass)
+				basemap.set_cell(x, y, grass)
+				if solid.getHash(x, y) >= 0.05:
+					solidmap.set_cell(x, y, tree)
 
 func simple_marching_squares(tilemap, grass, waterGrass):
 	for y in range(grid_start_y, grid_end_y):
@@ -81,5 +103,4 @@ func randomise_grass(tilemap, grass, grasses):
 		for x in range(grid_start_x, grid_end_x):
 			if grass == tilemap.get_cell(x, y):
 				tilemap.set_cell(x, y, grasses[rand_range(0,8)])
-
 
